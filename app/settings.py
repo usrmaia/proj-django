fa5b5e77661f6fb9958055710cd4f3b1d63be881
George Maia <georgemaiaf@gmail.com>
2025-09-25 07:42:15 -0300

    Remove configuração de ALLOWED_HOSTS para maior segurança

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&_s*$qckv56yt1*#81snb*k$dsdxhenhmshp-j+yby0zamj(2!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
